56847c7af00baeb147b4ade8b8a8760afcd43814
sunchutao <1050405528@qq.com>
2019-12-10 02:33:34 +0800

    1.1v

---
+++ HalliGalli/Classes/Model/Player.swift
@@ -0,0 +1,9 @@
+//
+//  Player.swift
+//  HalliGalli
+//
+//  Created by JASON on 2019/12/9.
+//  Copyright © 2019 HalliGalli. All rights reserved.
+//
+
+import Foundation
---
+++ HalliGalli/Classes/Model/Server.swift
@@ -0,0 +1,9 @@
+//
+//  Server.swift
+//  HalliGalli
+//
+//  Created by JASON on 2019/12/9.
+//  Copyright © 2019 HalliGalli. All rights reserved.
+//
+
+import Foundation
